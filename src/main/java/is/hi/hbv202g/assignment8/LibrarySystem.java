00071ba19f54dbbd35c0125fce8ca692ae27fc85
ThoraDis <142800101+ThoraDis@users.noreply.github.com>
2026-03-10 10:39:22 +0000

    Exception classes

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,3 +1,11 @@
 package is.hi.hbv202g.assignment8;
+
+import java.util.List;
+
 public class LibrarySystem {
+
+
+    public static void addBookWithTitleAndAuthorList(String title, List<Author> authors) throws EmptyAuthorListException{
+
+    }
 }
